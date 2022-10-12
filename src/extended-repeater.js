52b6b129f4264a1cd48162f8dfc2d17869b080dd
Nadya Baranova <12nadya11@gmail.com>
2022-10-12 20:42:42 +0300

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+
+	let array = Object.entries(options).flat();
+
+
+	let repeatTimes;
+	array.includes('repeatTimes') ? repeatTimes = array[array.indexOf('repeatTimes') + 1] : repeatTimes = '';
+	let separator;
+	array.includes('separator') ? separator = array[array.indexOf('separator') + 1] : separator = '+';
+	let addition;
+	array.includes('addition') ? addition = array[array.indexOf('addition') + 1] : addition = '';
+	let additionRepeatTimes;
+	array.includes('additionRepeatTimes') ? additionRepeatTimes = array[array.indexOf('additionRepeatTimes') + 1] : additionRepeatTimes = '';
+	let additionSeparator;
+	array.includes('additionSeparator') ? additionSeparator = array[array.indexOf('additionSeparator') + 1] : additionSeparator = '|';
+
+	let additionalPart = '';
+	if (array.includes('addition') ) {
+		additionalPart = addition;
+		for (let i = 0; i < additionRepeatTimes - 2; i++) {
+			additionalPart += additionSeparator + addition;
+		}
+		if (additionRepeatTimes > 1) {
+			additionalPart += additionSeparator + addition;
+		}
+	}
+
+
+	let mainPart
+
+
+	mainPart = str + additionalPart;
+	for (let i = 0; i < repeatTimes - 2; i++) {
+		mainPart += separator + str + additionalPart;
+	}
+
+	if (repeatTimes > 1) {
+		mainPart += separator + str + additionalPart
+	}
+
+	return mainPart
+
 }
 
 module.exports = {
-  repeater
+	repeater
 };
