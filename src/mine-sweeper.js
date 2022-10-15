5df94e86ce9e3abd2b10c60e6029272a9903d7f4
Nadya Baranova <12nadya11@gmail.com>
2022-10-15 13:33:21 +0300

    feat: add mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -24,9 +24,76 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]
  */
 function minesweeper(matrix) {
-  
+	let final = [];
+
+
+	function arrayCheckValue(value, valueTop, valueLeft) {
+		let arrayCheck = [];
+		if (matrix[valueTop - 1]) {
+			if (matrix[valueTop - 1][valueLeft - 1]) { matrix[valueTop - 1][valueLeft - 1] === true ? arrayCheck.push(true) : arrayCheck.push(false) }
+			if (matrix[valueTop - 1][valueLeft]) { matrix[valueTop - 1][valueLeft] === true ? arrayCheck.push(true) : arrayCheck.push(false) }
+			if (matrix[valueTop - 1][valueLeft + 1]) { matrix[valueTop - 1][valueLeft + 1] === true ? arrayCheck.push(true) : arrayCheck.push(false) }
+		}
+
+		if (matrix[valueTop][valueLeft - 1]) { matrix[valueTop][valueLeft - 1] === true ? arrayCheck.push(true) : arrayCheck.push(false) }
+		if (matrix[valueTop][valueLeft + 1]) { matrix[valueTop][valueLeft + 1] === true ? arrayCheck.push(true) : arrayCheck.push(false) }
+
+		if (matrix[valueTop + 1]) {
+			if (matrix[valueTop + 1][valueLeft - 1]) { matrix[valueTop + 1][valueLeft - 1] === true ? arrayCheck.push(true) : arrayCheck.push(false) }
+			if (matrix[valueTop + 1][valueLeft]) { matrix[valueTop + 1][valueLeft] === true ? arrayCheck.push(true) : arrayCheck.push(false) }
+			if (matrix[valueTop + 1][valueLeft + 1]) { matrix[valueTop + 1][valueLeft + 1] === true ? arrayCheck.push(true) : arrayCheck.push(false) }
+		}
+
+		if (arrayCheck.filter(item => item === true).length === 0) {
+			final.push(0)
+		} else {
+			final.push(arrayCheck.filter(item => item === true).length)
+		}
+	}
+
+	for (let i = 0; i < matrix.length; i++) {
+		for (let t = 0; t < matrix[0].length; t++) {
+			arrayCheckValue(matrix[i][t], i, t)
+		}
+	}
+
+	let answer = [];
+
+	if (matrix.length === 3) {
+		answer.push([]);
+		answer.push([]);
+		answer.push([]);
+		answer[0].push(final[0])
+		answer[0].push(final[1])
+		answer[0].push(final[2])
+		answer[1].push(final[3])
+		answer[1].push(final[4])
+		answer[1].push(final[5])
+		answer[2].push(final[6])
+		answer[2].push(final[7])
+		answer[2].push(final[8])
+	} else if (matrix.length === 2) {
+		answer.push([]);
+		answer.push([]);
+		answer[0].push(final[0])
+		answer[0].push(final[1])
+		answer[0].push(final[2])
+		answer[1].push(final[3])
+		answer[1].push(final[4])
+		answer[1].push(final[5])
+	}
+
+
+
+	console.log(answer)
+
+	return answer;
 }
 
+
+
+
+
 module.exports = {
-  minesweeper
+	minesweeper
 };
