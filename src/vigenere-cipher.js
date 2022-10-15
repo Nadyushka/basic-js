f2d2e86d2d5fd294cdc02ad1d0a338a8885bde79
Nadya Baranova <12nadya11@gmail.com>
2022-10-15 21:55:06 +0300

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,82 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+	constructor(value) {
+		if (!value) {
+			this.value = true;
+		} else {
+			this.value = false;
+		}
+	}
+
+
+	decrypt(word, key) {
+
+		if (!word || !key || word === undefined || key === undefined) {
+			throw new Error('Incorrect arguments!')
+		}
+
+		let alhabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		let result = '';
+		let keyWord = (key + key + key + key + key + key + key + key + key + key + key + key + key + key + key + key + key + key + key + key + key).slice(0, word.length).toUpperCase();
+
+		for (let i = 0, t = 0; i < word.length; i++, t++) {
+			let indexOfletter = +alhabet.indexOf(word[i].toUpperCase());
+			let indexOfKey = +alhabet.indexOf(keyWord[t].toUpperCase());
+
+			if (word[i] === ' ') {
+				result += ' ';
+				t--;
+			} else if ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', ":", '-', ',', '.', '&', '$', '#', '(', ')', '^', '*', '@', '/', '|', '%'].includes(word[i])) {
+				result += word[i];
+			} else {
+				let newValue = indexOfletter - indexOfKey;
+				if (newValue >= 0) {
+					result += alhabet[newValue]
+				} else {
+					result += alhabet[alhabet.length + newValue]
+				}
+			}
+		}
+		return result;
+	}
+
+	encrypt(word, key) {
+
+		if (!word || !key || word === undefined || key === undefined) {
+			throw new Error('Incorrect arguments!')
+		}
+
+		let alhabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		let alhabetNew = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		let result = '';
+		let keyWord = (key + key + key + key + key + key).slice(0, word.length);
+
+		for (let i = 0, t = 0; i < word.length; i++, t++) {
+			let indexOfletter = +alhabet.indexOf(word[i].toUpperCase());
+			let indexOfAlphabet = +alhabet.indexOf(keyWord[t].toUpperCase());
+
+			if (word[i] === ' ') {
+				result += ' ';
+				t--;
+			} else if ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', ":", '-', ',', '.', '&', '$', '#', '(', ')', '^', '*', '@', '/', '|', '%'].includes(word[i])) {
+				result += word[i];
+			} else {
+				let newValue = indexOfletter + indexOfAlphabet;
+				result += alhabetNew[newValue]
+			}
+
+		}
+
+		return result;
+
+	}
+
+
+
 }
 
 module.exports = {
-  VigenereCipheringMachine
+	VigenereCipheringMachine
 };
